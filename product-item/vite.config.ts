f1e60ed5c3951f70170e9a6791c7a0c5264780c1
andrew borzillo <dzillo1@gmail.com>
2025-05-23 11:15:24 -0400

    edits

--- product-item/vite.config.ts
+++ product-item/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-comp/', 
+  base: '/react-comp/product-item/',
   plugins: [react()],
+  build: {
+    outDir: '../../dist/product-item',
+    emptyOutDir: true,
+  },
 })
